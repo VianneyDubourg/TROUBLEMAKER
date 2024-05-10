53cd60a4caeca9fcf3e7e5933d5fda801f14586a
vianney dubourg <68192486+VianneyDubourg@users.noreply.github.com>
2024-05-10 10:05:37 +0200

    Add files via upload

---
+++ generate.py
@@ -0,0 +1,32 @@
+import markovify
+import serial
+import time
+
+# Fonction pour charger le fichier de prompt
+def load_prompt(file_path):
+    with open(file_path, "r", encoding="utf-8") as f:
+        text = f.read()
+    return text
+
+# Chemin vers votre fichier prompt.txt
+prompt_file = "prompt.txt"
+
+# Charger le fichier prompt
+text = load_prompt(prompt_file)
+
+# Créer le modèle Markovify
+model = markovify.Text(text)
+
+# Configuration du port série
+ser = serial.Serial('COM9', 9600)  # Remplacez 'COM3' par le port série de votre Arduino
+
+
+# Générer une phrase aléatoire
+sentence = model.make_sentence()
+
+# Envoyer la phrase sur le port série de l'Arduino
+ser.write((sentence + '\n').encode('utf-8'))
+
+# Attendre un court instant avant de générer la prochaine phrase
+time.sleep(1)
+print(sentence)
---
+++ serial.ino
@@ -0,0 +1,25 @@
+#include <SoftwareSerial.h>
+#include <Adafruit_Thermal.h>
+
+#define RX_PIN 5
+#define TX_PIN 6
+
+String prompt;
+
+SoftwareSerial mySerial(RX_PIN, TX_PIN); // RX, TX
+Adafruit_Thermal printer(&mySerial);
+
+void setup() {
+  Serial.begin(9600);
+  mySerial.begin(9600);  // Vitesse de communication série
+  printer.begin(); // Initialise l'imprimante thermique
+}
+
+void loop() {
+  
+  prompt = Serial.readString();
+
+  printer.println();  
+}
+
+    
