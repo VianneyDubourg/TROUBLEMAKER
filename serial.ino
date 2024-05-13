cfa468e46675c8777ef03a52eea9ec58667843a3
Valentin Baron <168755595+wikzyy@users.noreply.github.com>
2024-05-13 15:14:30 +0200

    Update serial.ino

--- serial.ino
+++ serial.ino
@@ -1,29 +1,29 @@
 #include <SoftwareSerial.h>
 #include <Adafruit_Thermal.h>
 
-#define RX_PIN 5 // Choix du port d'entrée
-#define TX_PIN 6 // Choix du port de sortie
+#define RX_PIN 5
+#define TX_PIN 6 
 
 String prompt;
 
 SoftwareSerial mySerial(RX_PIN, TX_PIN); // RX, TX
 Adafruit_Thermal printer(&mySerial);
 int heat_time = 1200000000;
-Adafruit_Thermal warm_up(heat_time); // Echauffement de l'imprimante
+Adafruit_Thermal warm_up(heat_time);
 
 void setup() {
   Serial.begin(9600);
   mySerial.begin(9600);  // Vitesse de communication série
-  printer.begin(); // Initialiser l'imprimante thermique
+  printer.begin(); // Initialise l'imprimante thermique
 }
 
 void loop() {
-    if (Serial.available() > 0) // Condition sur l'arrivée d'informations
+  if (Serial.available() > 0)
   {
-    prompt = Serial.readString(); // Lecture des données 
+    Adafruit_Thermal warm_up(heat_time);
+    prompt = Serial.readString();
+    printer.setLineHeight(20);
     printer.boldOn();
-    printer.println(prompt); // Impression des données
-    printer.println(); // Saut d'une ligne 
+    printer.println(prompt);
+} 
 }
-
-    
