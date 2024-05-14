f4ee98692759755375d0e9eb0954b4d859a894f6
Valentin Baron <168755595+wikzyy@users.noreply.github.com>
2024-05-14 15:47:28 +0200

    Update generate.py
    We add a function that can choose which file we will use to create and new sentence with a rotatory switch with 12 positions.
    For this, we ask to the Arduino to send values of different analog port to the computer. Then we read this values with python and if one of them is equal to 0 during 2s we choose a specific files, depending on which port respect this condition.

--- generate.py
+++ generate.py
@@ -2,14 +2,44 @@ import markovify
 import serial
 import time
 
-# Fonction pour charger le fichier de prompt
+# Connexion au arduino
+ser = serial.Serial('COM3', 9600)  # Assurez-vous de spécifier le bon port série
+ser.flush()
+
+# Fonction pour ouvrir la base de donnée
 def load_prompt(file_path):
     with open(file_path, "r", encoding="utf-8") as f:
         text = f.read()
     return text
 
-# Chemin vers votre fichier prompt.txt
-prompt_file = "prompt.txt"
+# Condition pour le choix de la base de données
+def check_condition(valeur):
+    start_time = time.time()
+    while time.time() - start_time < 2:
+        if condition_est_verifiee(valeur):
+            return True
+        time.sleep(0.5)
+    return False
+
+# Fonction pour vérifier la condition
+def condition_est_verifiee(valeur):
+    return valeur == 0
+
+# Boucle pour définir sur quelle base de donnée on travaille
+while True:
+    if ser.in_waiting > 0:
+        line = ser.readline().decode('utf-8').rstrip()
+        values = line.split(",")
+        analogValueA0 = int(values[0])
+        analogValueA1 = int(values[1])
+        analogValueA2 = int(values[2])
+        if check_condition(analogValueA0)==True:
+            prompt_file = "dinosaures.txt"
+        if check_condition(analogValueA1)==True:
+            prompt_file = "prompt.txt"
+        if check_condition(analogValueA2)==True:
+            prompt_file = "citations.txt"       
+        break
 
 # Charger le fichier prompt
 text = load_prompt(prompt_file)
@@ -17,18 +47,15 @@ text = load_prompt(prompt_file)
 # Créer le modèle Markovify
 model = markovify.Text(text)
 
-# Configuration du port série
-ser = serial.Serial('COM3', 9600)  # Remplacez 'COM3' par le port série de votre Arduino
-
 # Générer une phrase aléatoire
 sentence = model.make_sentence()
 
 # Attendre un court instant avant de générer la prochaine phrase
-time.sleep(2)
+time.sleep(1)
 
 # Envoyer une entête sur le port série de l'Arduino
 ser.write(("----------").encode())
-time.sleep(4)
+time.sleep(2)
 
 # Fonction pour couper les phrases en groupe de 4
 def splitTextToTriplet(string):
@@ -39,7 +66,7 @@ def splitTextToTriplet(string):
 # Envoyer la phrase sur le port série de l'Arduino par groupe de 4
 for groupe in splitTextToTriplet(str(sentence)):
     ser.write((groupe).encode())
-    time.sleep(4)
+    time.sleep(3)
 
 # Ecrire la phrase pour vérifier
 print(sentence)
