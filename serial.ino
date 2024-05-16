6845c817cc63a9e094f7807a67d9127e2ce8a848
Valentin Baron <168755595+wikzyy@users.noreply.github.com>
2024-05-16 09:28:37 +0200

    Update serial.ino
    We add port for the rotary switch (a total of 5 positions).

--- serial.ino
+++ serial.ino
@@ -1,8 +1,8 @@
 #include <SoftwareSerial.h>
 #include <Adafruit_Thermal.h>
 
-#define RX_PIN 5
-#define TX_PIN 6 
+#define RX_PIN 7
+#define TX_PIN 8
 
 String prompt;
 
@@ -15,6 +15,8 @@ void setup() {
   Serial.begin(9600);
   mySerial.begin(9600);  // Vitesse de communication série
   printer.begin(); // Initialise l'imprimante thermique
+  
+  //Choix des ports utilisés par le bouton
   pinMode(9, INPUT); // Ports en position de receveur
   pinMode(10, INPUT);
   pinMode(11, INPUT);
@@ -29,16 +31,22 @@ void loop() {
   int valeur0 = digitalRead(9);
   int valeur1 = digitalRead(10);
   int valeur2 = digitalRead(11);
+  int valeur3 = digitalRead(12);
+  int valeur4 = digitalRead(13);
 
   // Envoyer les valeurs via le port série
   Serial.print(valeur0);
   Serial.print(",");
   Serial.print(valeur1);
   Serial.print(",");
-  Serial.println(valeur2);
+  Serial.print(valeur2);
+  Serial.print(",");
+  Serial.print(valeur3);
+  Serial.print(",");
+  Serial.println(valeur4);
   delay(1000);
   
-  // Imprimer les phrases si on en reçoit une
+  // Imprimer les phrases si on en reçoit une phrase
   if (Serial.available() > 0)
   {
     Adafruit_Thermal warm_up(heat_time);
