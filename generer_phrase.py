3407f11588077c8114f02a9e47d0091674d4a616
Valentin Baron <168755595+wikzyy@users.noreply.github.com>
2024-05-16 14:57:16 +0200

    Update generer_phrase.py
    We modify the special case where we print a random sentence. 
    We also modify the game with dinosaur's nouns and we now use the basic Markov because we say to Markov that the state size of the data base is 1.

--- generer_phrase.py
+++ generer_phrase.py
@@ -2,11 +2,9 @@ import markovify
 import serial
 import time
 import random
-from markov_apprenant.py import MarkovChain, main
 
 # Connexion au arduino
 ser = serial.Serial('COM3', 9600)  # Assurez-vous de spécifier le bon port série
-ser.flush()
 
 # Fonction pour ouvrir la base de donnée
 def load_prompt(file_path):
@@ -22,61 +20,58 @@ while True:
         valeur0 = str(valeurs[0])
         valeur1 = str(valeurs[1])
         valeur2 = str(valeurs[2])
+        valeur3 = str(valeurs[3])
+        valeur4 = str(valeurs[4])
+        valeur5 = str(valeurs[5])
         if str(valeur0) == "0":
-            prompt_file = "actions_verites.txt"
+            prompt_file = "prompt.txt"
             break
         if str(valeur1) == "0":
-            prompt_file = "dinosaures_noms.txt"
+            prompt_file = "dinosaures.txt"
             break
         if str(valeur2) == "0":
-            prompt_file = "dinosaures_descriptions.txt"
+            prompt_file = "citations.txt"
             break
         if str(valeur3) == "0":
             prompt_file = "random.txt"
             break
         if str(valeur4) == "0":
-            prompt_file = "citations_celebres.txt"
+            prompt_file = "dinosaures_nom.txt"
             break
         if str(valeur5) == "0":
             prompt_file = "donnees_participants.txt"
-            break
         prompt_file = "rap.txt"
         break
-
-prompt_file = "dinosaures_nom.txt"
+        
+#Pour forcer le choix
+#prompt_file = "dinosaures_nom.txt"
 
 # Charger le fichier prompt
 text = load_prompt(prompt_file)
 
+# Cas particulier des noms de dinosaures
+if prompt_file == "dinosaures_nom.txt":
+    ser.write(("----------").encode())
+    time.sleep(2)
+    model = markovify.Text(text,state_size=1)
+    sentence = model.make_sentence()
+    sentence = sentence.replace(' ','')
+    ser.write((sentence).encode())
+    time.sleep(3)
+    print(prompt_file)
+    print(sentence)
+    exit()
+
 # Créer le modèle Markovify
 model = markovify.Text(text)
 
-# Générer une phrase aléatoire cas particulier nom de dinosaures
-if prompt_file == 'dinosaures_nom.txt':
-    sentence = main()
-    sentence = sentence.replace(' ','')
-    time.sleep(2)
-    ser.write(("----------").encode())
-    time.sleep(2)
-    ser.write((sentence).encode())
-    print(prompt_file)
-    print(sentence)
-    exit()
-
 # Générer une phrase aléatoire cas particulier mode random
 if prompt_file == "random.txt":
-    lines = open('actionverite.txt').read().splitlines()
+    lines = open('random.txt').read().splitlines()
     sentence = random.choice(lines) 
-    time.sleep(2)
-    ser.write(("----------").encode())
-    time.sleep(2)
-    ser.write((sentence).encode())
-    print(prompt_file)
-    print(sentence)
-    exit()
-
-# Générer une phrase aléatoire cas classique
-sentence = model.make_sentence()
+else:
+    # Générer une phrase aléatoire cas classique
+    sentence = model.make_sentence()
 
 # Attendre un court instant avant de générer la prochaine phrase
 time.sleep(2)
@@ -86,13 +81,13 @@ ser.write(("----------").encode())
 time.sleep(2)
 
 # Fonction pour couper les phrases en groupe de 4
-def splitTextToTriplet(string):
+def splitTextToQuadruplet(string):
     words = string.split()
     grouped_words = [' '.join(words[i: i + 4]) for i in range(0, len(words), 4)]
     return grouped_words
 
 # Envoyer la phrase sur le port série de l'Arduino par groupe de 4
-for groupe in splitTextToTriplet(str(sentence)):
+for groupe in splitTextToQuadruplet(str(sentence)):
     ser.write((groupe).encode())
     time.sleep(3)
 
