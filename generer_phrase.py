222c5bebd7845cfdd39d86aa81f6b8ba1bffa0b6
Valentin Baron <168755595+wikzyy@users.noreply.github.com>
2024-05-17 09:49:36 +0200

    Update generer_phrase.py
    We add comments on the code to explain it.

--- generer_phrase.py
+++ generer_phrase.py
@@ -7,12 +7,13 @@ import random
 ser = serial.Serial('COM3', 9600)  # Assurez-vous de spécifier le bon port série
 
 # Fonction pour ouvrir la base de donnée
-def load_prompt(file_path):
-    with open(file_path, "r", encoding="utf-8") as f:
-        text = f.read()
-    return text
+def charger_donnees(chemin):
+    with open(chemin, "r", encoding="utf-8") as f:
+        texte = f.read()
+    return texte
 
 # Boucle pour définir sur quelle base de donnée on travaille
+# Chaque valeurs correspond à la position de chaque boutons.
 while True:
     if ser.in_waiting > 0:
         line = ser.readline().decode('utf-8').rstrip()
@@ -23,75 +24,74 @@ while True:
         valeur3 = str(valeurs[3])
         valeur4 = str(valeurs[4])
         valeur5 = str(valeurs[5])
-        if str(valeur0) == "0":
-            prompt_file = "prompt.txt"
+        if valeur0 == "0":
+            base_de_donnees = "actions_verites.txt"
             break
-        if str(valeur1) == "0":
-            prompt_file = "dinosaures.txt"
+        if valeur1 == "0":
+            base_de_donnees = "dinosaures_descriptions.txt"
             break
-        if str(valeur2) == "0":
-            prompt_file = "citations.txt"
+        if valeur2 == "0":
+            base_de_donnees = "citations_celebres.txt"
             break
-        if str(valeur3) == "0":
-            prompt_file = "random.txt"
+        if valeur3 == "0":
+            base_de_donnees = "random.txt"
             break
-        if str(valeur4) == "0":
-            prompt_file = "dinosaures_nom.txt"
+        if valeur4 == "0":
+            base_de_donnees = "dinosaures_noms.txt"
             break
-        if str(valeur5) == "0":
-            prompt_file = "donnees_participants.txt"
-        prompt_file = "rap.txt"
+        if valeur5 == "0":
+            base_de_donnees = "donnees_participants.txt"
+        base_de_donnees = "rap.txt"
         break
-        
-#Pour forcer le choix
-#prompt_file = "dinosaures_nom.txt"
 
-# Charger le fichier prompt
-text = load_prompt(prompt_file)
+# Charger le fichier sélectionné
+texte = charger_donnees(base_de_donnees)
 
-# Cas particulier des noms de dinosaures
-if prompt_file == "dinosaures_nom.txt":
+# Cas particulier pour les noms de dinosaures
+# On spécifie la taille des la base de données
+# On supprime aussi les espaces entre chaque syllabes
+if base_de_donnees == "dinosaures_noms.txt":
     ser.write(("----------").encode())
     time.sleep(2)
-    model = markovify.Text(text,state_size=1)
-    sentence = model.make_sentence()
-    sentence = sentence.replace(' ','')
-    ser.write((sentence).encode())
+    modele = markovify.Text(texte,state_size=1)
+    phrase = modele.make_sentence()
+    phrase = phrase.replace(' ','')
+    ser.write((phrase).encode())
     time.sleep(3)
-    print(prompt_file)
-    print(sentence)
+    print(base_de_donnees)
+    print(phrase)
     exit()
 
-# Créer le modèle Markovify
-model = markovify.Text(text)
+# Créer le modèle de Markov
+modele = markovify.Text(texte)
 
 # Générer une phrase aléatoire cas particulier mode random
-if prompt_file == "random.txt":
-    lines = open('random.txt').read().splitlines()
-    sentence = random.choice(lines) 
+# On choisit une phrase aléatoire
+if base_de_donnees == "random.txt":
+    lignes = open('random.txt').read().splitlines()
+    phrase = random.choice(lignes) 
 else:
-    # Générer une phrase aléatoire cas classique
-    sentence = model.make_sentence()
-
-# Attendre un court instant avant de générer la prochaine phrase
-time.sleep(2)
+    # Générer une phrase aléatoire avec Markov cas classique
+    phrase = modele.make_sentence()
 
 # Envoyer un entête sur le port série de l'Arduino
 ser.write(("----------").encode())
+
+# Attendre un court instant avec de générer la phrase
 time.sleep(2)
 
 # Fonction pour couper les phrases en groupe de 4
-def splitTextToQuadruplet(string):
-    words = string.split()
-    grouped_words = [' '.join(words[i: i + 4]) for i in range(0, len(words), 4)]
-    return grouped_words
+def splitTextToQuadruplet(phrase):
+    mots = phrase.split()
+    mots_groupes = [' '.join(mots[i: i + 4]) for i in range(0, len(mots), 4)]
+    return mots_groupes
 
 # Envoyer la phrase sur le port série de l'Arduino par groupe de 4
-for groupe in splitTextToQuadruplet(str(sentence)):
+for groupe in splitTextToQuadruplet(str(phrase)):
     ser.write((groupe).encode())
     time.sleep(3)
 
 # Ecrire la phrase pour vérifier
-print(prompt_file)
-print(sentence)
+print(base_de_donnees)
+print(phrase)
 
